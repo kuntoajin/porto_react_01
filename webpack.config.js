434670a78f55850d2fd9e6145b655fbf79587e40
kuntoajin <kuntoaji.engineer@gmail.com>
2023-10-23 18:01:11 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports = [
+    {
+        entry: './src/client/index.tsx',
+        mode: 'developmemt',
+        target: 'web',
+        output: {
+            path: path.resolve(__dirname, 'dist/client'),
+            filename: 'client_bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader'
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|ttf)$/i,
+                    type: 'assets/resource'
+                }
+            ]
+        }
+    }
+]
