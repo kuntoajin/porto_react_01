c6d25aaeede71f3f0a6d665eb20b2668bab15561
kuntoajin <kuntoaji.engineer@gmail.com>
2023-10-24 17:33:27 +0700

    push

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = [
     {
-        entry: './src/client/index.tsx',
+        entry: './pages/index.tsx',
         mode: 'developmemt',
         target: 'web',
         output: {
