494f64f86fe0c664cb8b5bf248a9f8ddd0e7ba7e
kuntoajin <kuntoaji.engineer@gmail.com>
2023-11-08 10:39:11 +0700

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,5 @@
-console.log('test')
+import { createRoot } from 'react-dom/client';
+import React from 'react'
+
+const root = createRoot(document.getElementById('app') as HTMLElement)
+root.render(<h1>Hello, world</h1>);
